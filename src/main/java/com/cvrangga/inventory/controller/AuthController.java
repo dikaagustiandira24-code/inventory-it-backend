aa714e0686ace9d687f52b1882e3a8902a494bf3
Dika Agustiandira <dika2@komputerutama.rumahku.local>
2026-06-18 12:43:04 +0800

    fix: allow cross origin access for auth login

--- src/main/java/com/cvrangga/inventory/controller/AuthController.java
+++ src/main/java/com/cvrangga/inventory/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*") // <-- BARIS INI YANG KITA TAMBAHKAN
 public class AuthController {
 
     private final AuthService authService;
